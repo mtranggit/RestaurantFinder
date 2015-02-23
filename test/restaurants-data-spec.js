0decb929b54c2a70430bb68872009e6d7cfd4e50
Michael Trang <michael.trang@hotmail.com>
2015-02-23 06:23:55 +0000

    Fixed invalid referencing to MongoDB on localhost

--- test/restaurants-data-spec.js
+++ test/restaurants-data-spec.js
@@ -17,7 +17,7 @@ describe("Get restaurants", function () {
     var restaurants;
     
     before(function (done) {
-        restaurantsData.connectDB('mongodb://localhostssfs/restaurantfinder')
+        restaurantsData.connectDB('mongodb://localhost/restaurantfinder')
             .then(resetRestaurants)
             .then(restaurantsData.seedRestaurants)
             .then(restaurantsData.findRestaurants)
